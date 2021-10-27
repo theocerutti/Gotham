34dd4691c9d8c1221d0567e3a2aa2f8570271bc3
raphael-fournier <raphael1.fournier@epitech.eu>
2021-10-27 15:29:56 +0200

    init nest backend

--- backend/lib/gotham/mailer.ex
+++
@@ -1,3 +0,0 @@
-defmodule Gotham.Mailer do
-  use Swoosh.Mailer, otp_app: :gotham
-end
--- backend/lib/gotham/repo.ex
+++
@@ -1,5 +0,0 @@
-defmodule Gotham.Repo do
-  use Ecto.Repo,
-    otp_app: :gotham,
-    adapter: Ecto.Adapters.Postgres
-end
