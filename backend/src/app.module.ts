bb82662d201e7681e0f10e6d724c97d380248905
raphael-fournier <raphael1.fournier@epitech.eu>
2021-10-27 16:44:42 +0200

    init db postgres

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+      TypeOrmModule.forRoot({
+        type: 'postgres',
+        host: '',
+        port: 5432,
+        username: 'postgres',
+        password: 'postregs',
+        database: 'postgres',
+        entities: [],
+        synchronize: true,
+      }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
