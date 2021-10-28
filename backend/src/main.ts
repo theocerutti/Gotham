9fa24e473a4eb2c13ff866bd8fd95cf0d9cc7898
theocerutti <theo.cerutti@epitech.eu>
2021-10-28 10:45:47 +0200

    Add docker-compose for postgresql

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,25 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
 import {ValidationPipe} from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Setup Swagger
+/*  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);*/
+
+  // Setup class-validator
   app.useGlobalPipes(new ValidationPipe());
+
+  // Set /api route everywhere
   app.setGlobalPrefix('/api');
   await app.listen(4000);
 }
+
 bootstrap();
