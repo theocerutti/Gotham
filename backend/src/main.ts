72dd700dfb60589b555fe5337aba5049c41deb0d
theocerutti <theo.cerutti@epitech.eu>
2021-10-28 10:57:14 +0200

    Add Swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,24 +1,23 @@
 import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
 import {ValidationPipe} from "@nestjs/common";
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // Setup Swagger
-/*  const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
-    .setVersion('1.0')
-    .addTag('cats')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('swagger', app, document);*/
+  // Set /api route everywhere
+  app.setGlobalPrefix('/api');
 
   // Setup class-validator
   app.useGlobalPipes(new ValidationPipe());
 
-  // Set /api route everywhere
-  app.setGlobalPrefix('/api');
+  // Setup Swagger
+  const config = new DocumentBuilder()
+    .setTitle('TimeManger')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
   await app.listen(4000);
 }
 
