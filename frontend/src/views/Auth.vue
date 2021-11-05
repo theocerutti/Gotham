405fecc8f12c302b5408c1053d39717deb7b3046
theocerutti <theo.cerutti@epitech.eu>
2021-11-05 15:25:44 +0100

    Remove Auth Cards

--- frontend/src/views/Auth.vue
+++ frontend/src/views/Auth.vue
@@ -2,70 +2,66 @@
   <div class="container">
     <div v-if="currentAction === 'login'">
       <v-container fluid>
-        <v-card height="490px" width="1000px" style="padding: 15px">
-          <v-row dense>
-            <v-col :cols="6">
-              <v-card class="container" width="500px" height="400px">
-                <v-card-title>Login</v-card-title>
-                <v-card-text>Sign in to your account</v-card-text>
-                <v-text-field v-model="username" solo prepend-inner-icon="mdi-account"
-                              placeholder="Username"></v-text-field>
-                <v-text-field v-model="password" solo prepend-inner-icon="mdi-lock"
-                              placeholder="Password"></v-text-field>
-                <v-btn @click="signIn" color="#64aacf">
-                  sign in
-                </v-btn>
-              </v-card>
-            </v-col>
-            <v-col :cols="6">
-              <v-card width="440px" height="400px" class="container" color="#64aacf">
-                <v-card-title class="justify-center">Sign up</v-card-title>
-                <v-card-text class="text-center">
-                  If you did not already registered, you can create account by clicking here !
-                </v-card-text>
-                <v-btn @click="currentAction = 'register'" color="#497d98">
-                  Sign up now !
-                </v-btn>
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-card>
+        <v-row dense>
+          <v-col :cols="6">
+            <v-card class="container" width="500px" height="400px">
+              <v-card-title>Login</v-card-title>
+              <v-card-text align="center">Sign in to your account</v-card-text>
+              <v-text-field v-model="username" solo prepend-inner-icon="mdi-account"
+                            placeholder="Username"></v-text-field>
+              <v-text-field v-model="password" solo prepend-inner-icon="mdi-lock"
+                            placeholder="Password"></v-text-field>
+              <v-btn class="mb-5" @click="signIn" color="#64aacf">
+                sign in
+              </v-btn>
+            </v-card>
+          </v-col>
+          <v-col :cols="6">
+            <v-card width="440px" height="400px" class="container" color="#64aacf">
+              <v-card-title class="justify-center">Sign up</v-card-title>
+              <v-card-text class="text-center">
+                If you did not already registered, you can create account by clicking here !
+              </v-card-text>
+              <v-btn @click="currentAction = 'register'" color="#497d98">
+                Sign up now !
+              </v-btn>
+            </v-card>
+          </v-col>
+        </v-row>
       </v-container>
     </div>
     <div v-else>
       <v-container fluid>
-        <v-card style="padding: 15px">
-          <v-row dense>
-            <v-col :cols="6">
-              <v-card class="container" width="500px" height="500px">
-                <v-card-title>Register</v-card-title>
-                <v-card-text>Sign up</v-card-text>
-                <v-text-field v-model="username" :rules="rulesUsername" solo prepend-inner-icon="mdi-account"
-                              placeholder="Username"></v-text-field>
-                <v-text-field v-model="email" :rules="rulesEmail" solo prepend-inner-icon="mdi-email"
-                              placeholder="Email"></v-text-field>
-                <v-text-field v-model="password" type='password' solo prepend-inner-icon="mdi-lock"
-                              placeholder="Password"></v-text-field>
-                <v-text-field v-model="rePassword" type='password' :rules="[passwordConfirmationRule]" solo
-                              prepend-inner-icon="mdi-lock" placeholder="Password confirmation"></v-text-field>
-                <v-btn @click="signUp" style="margin-left: 38%;" color="#64aacf">
-                  sign up
-                </v-btn>
-              </v-card>
-            </v-col>
-            <v-col :cols="6">
-              <v-card width="440px" height="500px" class="container" color="#64aacf">
-                <v-card-title class="justify-center">Sign in</v-card-title>
-                <v-card-text class="text-center">
-                  If you already have an account, you can sign in here !
-                </v-card-text>
-                <v-btn @click="currentAction = 'login'" style="margin-left: 31%;" color="#497d98">
-                  Sign in now !
-                </v-btn>
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-card>
+        <v-row dense>
+          <v-col :cols="6">
+            <v-card class="container" width="500px" height="500px">
+              <v-card-title>Register</v-card-title>
+              <v-card-text align="center">Sign up</v-card-text>
+              <v-text-field v-model="username" :rules="rulesUsername" solo prepend-inner-icon="mdi-account"
+                            placeholder="Username"></v-text-field>
+              <v-text-field v-model="email" :rules="rulesEmail" solo prepend-inner-icon="mdi-email"
+                            placeholder="Email"></v-text-field>
+              <v-text-field v-model="password" type='password' solo prepend-inner-icon="mdi-lock"
+                            placeholder="Password"></v-text-field>
+              <v-text-field v-model="rePassword" type='password' :rules="[passwordConfirmationRule]" solo
+                            prepend-inner-icon="mdi-lock" placeholder="Password confirmation"></v-text-field>
+              <v-btn class="mb-5" @click="signUp" color="#64aacf">
+                sign up
+              </v-btn>
+            </v-card>
+          </v-col>
+          <v-col :cols="6">
+            <v-card width="440px" height="500px" class="container" color="#64aacf">
+              <v-card-title class="justify-center">Sign in</v-card-title>
+              <v-card-text class="text-center">
+                If you already have an account, you can sign in here !
+              </v-card-text>
+              <v-btn @click="currentAction = 'login'" color="#497d98">
+                Sign in now !
+              </v-btn>
+            </v-card>
+          </v-col>
+        </v-row>
       </v-container>
     </div>
   </div>
