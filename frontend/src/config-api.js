962968b4398518cd044a0b3d29c79a339591f013
theocerutti <theo.cerutti@epitech.eu>
2021-11-08 16:57:24 +0100

    Update env

--- frontend/src/config-api.js
+++ frontend/src/config-api.js
@@ -1,7 +1,7 @@
 import Axios from "axios";
 import store from "../src/store/index";
 
-const SERVER_URL = "http://localhost:4000/"; // TODO: get from env
+const SERVER_URL = process.env.VUE_APP_API_URL;
 
 export const api = Axios.create({
   transformRequest: [(data, headers) => {
